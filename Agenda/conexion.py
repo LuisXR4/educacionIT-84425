fea47ee4ed9537f76608b5f2c9cd858461e31cdf
LuisXR4 <mrcosmico@gmail.com>
2026-04-22 21:56:04 -0300

    Empece Agenda

---
+++ Agenda/conexion.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+def obtener_conexion():
+    try:
+        conexion = psycopg2.connect(
+            host="localhost",
+            database="agenda_db",
+            user="postgres",
+            password="Winner77",
+            port="5432"
+        )
+        print("✅ Conexión exitosa a PostgreSQL")
+        return conexion
+
+    except Exception as e:
+        print("❌ Error al conectar:", e)
+        return None
---
+++ Agenda/test_conexion.py
@@ -0,0 +1,9 @@
+from conexion import obtener_conexion
+
+conn = obtener_conexion()
+
+if conn:
+    print("TODO OK 🚀")
+    conn.close()
+else:
+    print("Fallo la conexión ❌")
